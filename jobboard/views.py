41c0f1fdb883bc75c517896e51a6e772381dc6a5
Eden Maor <edenmaor95@gmail.com>
2021-11-05 17:33:56 +0200

    Creating database migrations and test data

--- jobboard/views.py
+++ jobboard/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Message
+from .forms import MessageForm
+
 
 def board(request):
     messages = Message.objects.order_by('-date')
-    return render(request, 'jobboard/board.html')
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('board')
+    else:
+        form = MessageForm()
+    return render(request, 'jobboard/board.html', {
+        'messages': messages,
+        'form': form,
+    })
