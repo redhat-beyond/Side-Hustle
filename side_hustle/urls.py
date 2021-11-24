bfb712d46e8dd0ea11e04553d00468759097060c
AshkenYariv <ashkenazy.jariv@gmail.com>
2021-11-24 08:55:18 +0200

    Create user model, migration files and tests
    Creating a user model, a static method for creating users.
    Made migration files for user.
    Wrote tests for user creation.

--- side_hustle/urls.py
+++ side_hustle/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from jobboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.board, name='board')
 ]
