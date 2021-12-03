9a03cf6f84fb5aa7b8f300b63a2ed7686d004b9d
Assaf Gorovici <assafgorovici@gmail.com>
2021-12-04 00:27:17 +0200

    Add Job class

---
+++ jobs/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.conf import settings
+
+
+# Enum for Job Types
+class JobType(models.TextChoices):
+    FULL_TIME = '1', 'Full time'
+    PART_TIME = '2', 'Part time'
+    INTERNSHIP = '3', 'Internship'
+
+class Location(models.TextChoices):
+    Tel_Aviv = '1', 'Tel Aviv'
+    Jerusalem = '2', 'Jerusalem'
+    Haifa = '3', 'Haifa'
+
+# Jobs Model
+class Job(models.Model):
+    publisher = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.RESTRICT,
+        related_name='%(class)s_author'
+    )
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    job_type = models.CharField(
+        max_length=15,
+        choices=JobType.choices,
+        default=JobType.PART_TIME,
+        blank=True, null=True
+    )
+    location = models.CharField(
+        max_length=15,
+        choices=Location.choices,
+        default=Location.Tel_Aviv,
+        blank=True, null=True
+    )
+    company_name = models.CharField(max_length=255)
+    post_until = models.DateField()
+    is_active = models.BooleanField()
+    marked_count = models.IntegerField()
+    apply_link = models.URLField(max_length=200)
+
+    # Function to create a new job
+    @classmethod
+    def create_job(cls, publisher, title, description, job_type, location, company_name,
+                   post_until, is_active, marked_count, apply_link):
+        job = cls(publisher=publisher, title=title, description=description,
+                  job_type=job_type, location=location, company_name=company_name,
+                  post_until=post_until, is_active=is_active, marked_count=marked_count, apply_link=apply_link)
+        job.save()
+        return job
