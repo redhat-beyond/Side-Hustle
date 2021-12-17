7310edfd26de2c1f5f3613026ed48b870d651b5e
Shay Rachmany <shayakrach@gmail.com>
2021-12-17 13:13:31 +0200

    Refator urls
    Moving all the job urls to jobs/urls.py
    Including those urls in urls.py in one line.

--- side_hustle/urls.py
+++ side_hustle/urls.py
@@ -1,16 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.urls import path
 from django.urls import include
-from jobs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.JobView.as_view(), name="jobs"),
-    path('jobs', views.JobView.as_view(), name="jobs"),
-    path('jobs/<int:pk>', views.JobsDetailedView.as_view(), name="jobs_detailed"),
-    path('jobs/add/', views.AddJobsView.as_view(), name="jobs_add"),
-    path('jobs/edit/<int:pk>', views.UpdateJobsView.as_view(), name="jobs_edit"),
-    path('jobs/delete/<int:pk>', views.DeleteJobsView.as_view(), name="jobs_delete"),
     path('users/', include('django.contrib.auth.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('jobs/', include('jobs.urls'))
 ]
