c4458da202d570c79efabcf0d2cb44d0cb39958a
Shay Rachmany <shayakrach@gmail.com>
2021-12-17 13:18:21 +0200

    Add home page and set as default page
    Setting home page as the default page instead of the jobs page since
    the jobs page should only be visible to logged-in users.

--- side_hustle/urls.py
+++ side_hustle/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.urls import path
 from django.urls import include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
-    path('jobs/', include('jobs.urls'))
+    path('jobs/', include('jobs.urls')),
+    path('', views.homepage, name='home'),
+    path('home/', views.homepage, name='home'),
 ]
